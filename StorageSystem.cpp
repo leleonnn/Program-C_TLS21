2667523ff4e7f3f344708fdeb207d2c491704372
Leonard <72289543+leleonnn@users.noreply.github.com>
2021-10-27 23:07:41 +0700

    commit final

--- StorageSystem.cpp
+++ StorageSystem.cpp
@@ -5,34 +5,35 @@ using namespace std;
 int CategoryTotal=0;
 int DataTotal[1]={0};
 string Storage[999][999];
-void store(), open();
+int store(), open(), terminatecode();
 
 ///
 
-int terminateCode(){
-    return 0;
-}
-
-void menuOption(){
+int menuOption(){
     string Opt;
     cout<<"________________________________"<<endl<<"Choose Option (store/open/close)"<<endl;
     cin>>Opt;
     if(Opt=="store"){
-            store();
+        store();
     }else if(Opt=="open"){
-            open();
+        open();
     }else if(Opt=="close"){
-            cout<<"[Closing...]"; 
-            terminateCode();
+        terminateCode();
     }else{
-            cout<<"Error"<<endl;
-            menuOption();
+        cout<<"Error"<<endl;
+        menuOption();
     }
+    return 0;
+}
+
+int terminateCode(){
+    cout<<"[Closing...]";  
+    return 0;
 }
 
 ///
 
-void storeIn(int x){
+int storeIn(int x){
     string Data;
     cout<<endl<<"Store Your Data!"<<endl;
     cin>>Data;
@@ -40,49 +41,56 @@ void storeIn(int x){
     Storage[x][DataTotal[x]]=Data;
     cout<<"[Data Stored]"<<endl;
     menuOption();
+    return 0;
 }
 
-void store(){
+int store(){
     string Category;
     cout<<endl<<"Choose Category!"<<endl;
     cin>>Category;
     for(int i=0; i<=CategoryTotal; i++){
         if(Category==Storage[i][0]){
             storeIn(i);
+            return 0;
         }
     }
     CategoryTotal++;
     Storage[CategoryTotal][0]=Category;
     storeIn(CategoryTotal);
+    return 0;
 }
 
 ///
 
-void openIn(int x){
+int openIn(int x){
     string Data;
     cout<<endl<<"Open Data"<<endl;
     cin>>Data;
     for(int i=0; i<=DataTotal[x];i++){
-        if(Data==Storage[x][DataTotal[x]]){
+        if(Data==Storage[x][i]){
             cout<<"[Opening "<<Data<<"]"<<endl<<"..."<<endl;
             menuOption();
+            return 0;
         }
     }
     cout<<"[Error] Data Not Found"<<endl;
     menuOption();
+    return 0;
 }
 
-void open(){
+int open(){
     string Category;
     cout<<endl<<"Open Category"<<endl;
     cin>>Category;
     for(int i=0; i<=CategoryTotal;i++){
         if(Category==Storage[i][0]){
             openIn(i);
+            return 0;
         }
     }
     cout<<"[Error] Category Not Found"<<endl;
     menuOption();
+    return 0;
 }
 
 ///
